5d1591400a58c19d9588995c9b4b416821e58812
demurelian <demurelian@gmail.com>
2024-03-20 01:31:36 +0300

    Привязка свойства статус к тексту в статус баре

--- WPFMVVM/ViewModels/MainWindowViewModel.cs
+++ WPFMVVM/ViewModels/MainWindowViewModel.cs
@@ -9,6 +9,7 @@ namespace WPFMVVM.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        #region Заголовок окна
         private string _Title = "Анализ статистики";
         /// <summary>Заголовок окна</summary>
         public string Title
@@ -16,5 +17,16 @@ namespace WPFMVVM.ViewModels
             get => _Title;
             set => Set(ref _Title, value);
         }
+        #endregion
+        #region Статус программы
+        private string _Status = "Готов";
+        ///<summary>Статус программы</summary>
+        public string Status
+        {
+            get => _Status;
+            set => Set(ref _Status, value);
+        }
+        #endregion
+
     }
 }
