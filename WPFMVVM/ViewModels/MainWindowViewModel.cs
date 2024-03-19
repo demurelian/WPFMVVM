93b0fd49e1cbf59b6b933ef3bfd8d49f4bbdf9ee
demurelian <demurelian@gmail.com>
2024-03-20 01:17:26 +0300

    Тестовая разметка главного окна

--- WPFMVVM/ViewModels/MainWindowViewModel.cs
+++ WPFMVVM/ViewModels/MainWindowViewModel.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WPFMVVM.ViewModels.Base;
 
 namespace WPFMVVM.ViewModels
 {
-    internal class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModel
     {
+        private string _Title = "Анализ статистики";
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
     }
 }
