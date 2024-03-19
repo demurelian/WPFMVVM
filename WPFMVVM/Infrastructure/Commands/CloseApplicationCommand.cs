eaaa38c74813695f4623fabd501ed595f41198ec
demurelian <demurelian@gmail.com>
2024-03-20 02:30:02 +0300

    Определили класс команды закрытия приложения

---
+++ WPFMVVM/Infrastructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+using WPFMVVM.Infrastructure.Commands.Base;
+
+namespace WPFMVVM.Infrastructure.Commands
+{
+    internal class CloseApplicationCommand : Command
+    {
+        public override bool CanExecute(object? parameter) => true;
+        public override void Execute(object? parameter) => Application.Current.Shutdown();
+    }
+}
