45b4ab0ff1ccd50ecd8c91e05f59a7f7c8a0eaf5
demurelian <demurelian@gmail.com>
2024-03-21 01:59:13 +0300

    Получение первой строки из файла (все даты)

--- Tests/WMConsole/Program.cs
+++ Tests/WMConsole/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Globalization;
+using System.Net.NetworkInformation;
+using System.Linq;
 
 namespace WMConsole
 {
@@ -6,11 +9,35 @@ namespace WMConsole
     {
         private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
-        static void Main(string[] args)
+        private static async Task<Stream> GetDataStream()
         {
             var client = new HttpClient();
+            //нам интересны в первую очередь заголовки
+            var response = await client.GetAsync(data_url, HttpCompletionOption.ResponseHeadersRead);
+            return await response.Content.ReadAsStreamAsync();
+        }
+        private static IEnumerable<string> GetDataLines()
+        {
+            using var data_stream = GetDataStream().Result;
+            using var data_reader = new StreamReader(data_stream);
 
-            var csv_str = client.GetStringAsync(data_url).Result;
+            while(!data_reader.EndOfStream)
+            {
+                var line = data_reader.ReadLine();
+                if (string.IsNullOrWhiteSpace(line)) continue;
+                yield return line;
+            }
+        }
+        private static DateTime[] GetDates() => GetDataLines()
+            .First()
+            .Split(',')
+            .Skip(4)
+            .Select(s => DateTime.Parse(s, CultureInfo.InvariantCulture))//культура даты - формат, независимый от системы
+            .ToArray();
+        static void Main(string[] args)
+        {
+            var dates = GetDates();
+            Console.WriteLine(string.Join("\r\n", dates));
 
             Console.ReadLine();
         }
