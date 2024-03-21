3e9f29e5d5be0a47c79efc84163eaa2604fb42cd
demurelian <demurelian@gmail.com>
2024-03-21 04:03:44 +0300

    Добавляем тестовые данные с моделью, подключаем пакет для их визуализации

---
+++ WPFMVVM/Models/Country.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFMVVM.Models
+{
+    internal class PlaceInfo
+    {
+        public string Name { get; set; }
+        public Point Location { get; set; }
+        public IEnumerable<ConfirmedCount> Counts { get; set; }
+    }
+    internal class CountryInfo : PlaceInfo
+    {
+        public IEnumerable<ProvinceInfo> Provinces { get; set; }
+    }
+    internal class ProvinceInfo : PlaceInfo { }
+    internal struct ConfirmedCount
+    {
+        public DateTime Date { get; set; }
+        public int Count { get; set; }
+    }
+    internal struct DataPoint
+    {
+        public double XValue { get; set; }
+        public double YValue { get; set; }
+    }
+}
--- WPFMVVM/ViewModels/MainWindowViewModel.cs
+++ WPFMVVM/ViewModels/MainWindowViewModel.cs
@@ -6,12 +6,20 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using WPFMVVM.Infrastructure.Commands;
+using WPFMVVM.Models;
 using WPFMVVM.ViewModels.Base;
 
 namespace WPFMVVM.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        private IEnumerable<DataPoint> _TestDataPoints;
+        /// <summary>Тестовый набор данных для визуализации графиков</summary>
+        public IEnumerable<DataPoint> TestDataPoints 
+        {
+            get => _TestDataPoints;
+            set => Set(ref _TestDataPoints, value);
+        }
         #region Заголовок окна
         private string _Title = "Анализ статистики";
         /// <summary>Заголовок окна</summary>
@@ -45,6 +53,15 @@ namespace WPFMVVM.ViewModels
             #region Команды
             CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecute, CanCloseApplicationCommandExecute);
             #endregion
+
+            var data_points = new List<DataPoint>((int)(360 / 0.1));
+            for (var x = 0d; x <= 360; x+= 0.1)
+            {
+                const double to_rad = Math.PI / 180;
+                var y = Math.Sin(x * to_rad);
+                data_points.Add(new DataPoint { XValue = x, YValue = y });
+            }
+            TestDataPoints = data_points;
         }
     }
 }
