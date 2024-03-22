5e69141ab02352e4beeea1fe8a855efebf1faccc
demurelian <demurelian@gmail.com>
2024-03-22 03:57:05 +0300

    Тестовая предметная область

---
+++ WPFMVVM/Models/Decanat/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFMVVM.Models.Decanat
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        //Отчество
+        public string Patronymic { get; set; }
+        public DateTime Birthday { get; set; }
+        public double Rating { get; set; }
+    }
+    internal class Group
+    {
+        public string Name { get; set; }
+        public ICollection<Student> Students { get; set; }
+    }
+}
