4a891c098611f1a26bc80daaa11a4a02cb7588e7
demurelian <demurelian@gmail.com>
2024-03-25 23:32:03 +0300

    Базовая логика VM статистики стран

--- WPFMVVM/ViewModels/CountriesStatisticViewModel.cs
+++ WPFMVVM/ViewModels/CountriesStatisticViewModel.cs
@@ -1,4 +1,8 @@
-﻿using WPFMVVM.Services;
+﻿using System.Security.Cryptography.Pkcs;
+using System.Windows.Input;
+using WPFMVVM.Infrastructure.Commands;
+using WPFMVVM.Models;
+using WPFMVVM.Services;
 using WPFMVVM.ViewModels.Base;
 
 namespace WPFMVVM.ViewModels
@@ -7,11 +11,31 @@ namespace WPFMVVM.ViewModels
     {
         private DataService _DataService;
         private MainWindowViewModel _MainModel { get; }
+
+        private IEnumerable<CountryInfo> _Countries;
+
+        public IEnumerable<CountryInfo> Countries
+        {
+            get => _Countries;
+            private set => Set(ref _Countries, value);
+        }
+
+        #region Команды
+        public ICommand RefreshDataCommand { get; }
+
+        private void OnRefreshDataCommandExecute(object p)
+        {
+            Countries = _DataService.GetData();
+        }
+        #endregion
         public CountriesStatisticViewModel(MainWindowViewModel MainModel)
         {
             _MainModel = MainModel;
 
             _DataService = new DataService();
+            #region Команды
+            RefreshDataCommand = new LambdaCommand(OnRefreshDataCommandExecute);
+            #endregion
         }
     }
 }
