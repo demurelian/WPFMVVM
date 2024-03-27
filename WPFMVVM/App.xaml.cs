82085c39243df3035861604ee18b2ba81cd7a1e7
demurelian <demurelian@gmail.com>
2024-03-27 03:59:55 +0300

    Инфраструктура для создания хоста приложения

--- WPFMVVM/App.xaml.cs
+++ WPFMVVM/App.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Windows;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Windows;
 using WPFMVVM.Services;
+using WPFMVVM.ViewModels;
 
 namespace WPFMVVM
 {
@@ -12,5 +15,11 @@ namespace WPFMVVM
             IsDesignMode = false;
             base.OnStartup(e);
         }
+    }
+
+    public static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+    {
+        services.AddSingleton<DataService>();
+        services.AddSingleton<CountriesStatisticViewModel>();
     }
 }
--- WPFMVVM/Program.cs
+++ WPFMVVM/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,11 +11,27 @@ namespace WPFMVVM
     public static class Program
     {
         [STAThread]
-        public static void Main()
+        public static void Main(string[] args)
         {
             var app = new App();
             app.InitializeComponent();
             app.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var host_builder = Host.CreateDefaultBuilder(args);
+
+            host_builder.UseContentRoot(Environment.CurrentDirectory);
+            host_builder.ConfigureAppConfiguration((host, cfg) =>
+            {
+                cfg.SetBasePath(Environment.CurrentDirectory);
+                cfg.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            });
+
+            host_builder.ConfigureServices(App.ConfigureServices);
+
+            return host_builder;
         }
     }
 }
