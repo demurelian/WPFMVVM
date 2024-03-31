3ab6c2768f254d9c35e4cbd07348812665a0fed2
demurelian <demurelian@gmail.com>
2024-03-31 03:21:55 +0300

    Создание файлов + методов расширения для коллекции сервисов

---
+++ WPFMVVM/Services/ServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using WPFMVVM.Services.Interfaces;
+
+namespace WPFMVVM.Services
+{
+    internal static class Registrator
+    {
+        public static IServiceCollection RegisterServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IDataService, DataService>();
+
+            return services;
+        }
+    }
+}
