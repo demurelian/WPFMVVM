7852f251e2b857e2cb764157f2ac01f880b940a0
demurelian <demurelian@gmail.com>
2024-04-09 10:34:22 +0300

    Создание тестовых потоков

--- Tests/WMConsole/Program.cs
+++ Tests/WMConsole/Program.cs
@@ -1,60 +1,42 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Threading;
 
 namespace WMConsole
 {
     class Program
     {
-        private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
-
-        private static async Task<Stream> GetDataStream()
+        public static void Main(string[] args)
         {
-            var client = new HttpClient();
-            //нам интересны в первую очередь заголовки
-            var response = await client.GetAsync(data_url, HttpCompletionOption.ResponseHeadersRead);
-            return await response.Content.ReadAsStreamAsync();
-        }
-        private static IEnumerable<string> GetDataLines()
-        {
-            using var data_stream = GetDataStream().Result;
-            using var data_reader = new StreamReader(data_stream);
+            Thread.CurrentThread.Name = "Main thread";
 
-            while(!data_reader.EndOfStream)
+            var thread = new Thread(ThreadMethod);
+            thread.Name = "Other thread";
+            thread.Start();
+
+            CheckThread();
+            
+            for(int i = 0; i < 100; i++)
             {
-                var line = data_reader.ReadLine();
-                if (string.IsNullOrWhiteSpace(line)) continue;
-                yield return line.Replace("Korea,","Korea - ");
+                Console.WriteLine(i);
+                Thread.Sleep(100);
             }
         }
-        private static DateTime[] GetDates() => GetDataLines()
-            .First()
-            .Split(',')
-            .Skip(4)//Провинция, страна, долгота, широта
-            .Select(s => DateTime.Parse(s, CultureInfo.InvariantCulture))//культура даты - формат, независимый от системы
-            .ToArray();
 
-        //Плюс типа IEnumerable - ленивые методы, которые не тянут все данные
-        //Тут используем кортеж данных
-        private static IEnumerable<(string Country, string Province, int[] Counts)> GetData()
+        private static void ThreadMethod()
         {
-            var lines = GetDataLines()
-                .Skip(1)//заголовок
-                .Select(line => line.Split(','));
-            foreach(var row in lines)
+            CheckThread();
+
+            while(true)
             {
-                var province = row[0].Trim();
-                var country = row[1].Trim(' ','"');
-                var counts = row.Skip((row[4].Equals("-68.2385")) ? 5 : 4)//Очень странный баг, что заходило в 4й столбец с координатой в одном месте
-                    .Select(int.Parse)
-                    .ToArray();
-                yield return (country, province, counts);
+                Thread.Sleep(100);
+                Console.Title = DateTime.Now.ToString();
             }
         }
-        static void Main(string[] args)
-        {
-            var russia_data = GetData().First(v => v.Country.Equals("Russia", StringComparison.OrdinalIgnoreCase));
-            Console.WriteLine(string.Join("\r\n", GetDates().Zip(russia_data.Counts, (date, count) => $"{date:dd-MM}: {count}")));
 
-            Console.ReadLine();
+        private static void CheckThread()
+        {
+            var thread = Thread.CurrentThread;
+            Console.WriteLine("{0}:{1}", thread.ManagedThreadId, thread.Name);
         }
     }
 }
