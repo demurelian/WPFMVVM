3ba1b88dc1ca0d6681d8cac0e7056d92ab32016d
demurelian <demurelian@gmail.com>
2024-04-09 16:56:33 +0300

    Передача параметров в поток

--- Tests/WMConsole/Program.cs
+++ Tests/WMConsole/Program.cs
@@ -11,7 +11,14 @@ namespace WMConsole
 
             var thread = new Thread(ThreadMethod);
             thread.Name = "Other thread";
-            thread.Start();
+
+            thread.Start(42);
+
+            var count = 5;
+            var msg = "Hello";
+            var timeout = 250;
+
+            new Thread(() => PrintMethod(msg, count, timeout)) { IsBackground = true }.Start();
 
             CheckThread();
             
@@ -22,8 +29,20 @@ namespace WMConsole
             }
         }
 
-        private static void ThreadMethod()
+        private static void PrintMethod(string Message, int Count, int Timeout)
         {
+            for (int i = 0; i < Count;i++)
+            {
+                Console.WriteLine(Message);
+                Thread.Sleep(Timeout);
+            }
+        }
+
+        private static void ThreadMethod(object parametr)
+        {
+            var value = (int)parametr;
+            Console.WriteLine(value);
+
             CheckThread();
 
             while(true)
